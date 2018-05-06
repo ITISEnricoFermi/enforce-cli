6dfebc9779ef122ea5a86f025c2641ddd2f65b8d
Ernesto Montada Romero <e.montada0@gmail.com>
2018-05-06 14:24:19 +0200

    Motors and sensors options

---
+++ commands.js
@@ -0,0 +1,35 @@
+const yargs = require("yargs")
+
+const args = yargs
+// Motors
+.options({
+	l: {
+		alias: "left",
+		string: true
+	},
+	r: {
+		alias: "right",
+		string: true
+	}
+})
+// Sensors
+.options({
+	g: {
+		alias: "gps",
+		string: true
+	},
+	i: {
+		alias: "imu",
+		string: true
+	},
+	t: {
+		alias: "thp",
+		string: true
+	}
+})
+// TODO: Camera
+.options({
+
+})
+
+module.exports = args
