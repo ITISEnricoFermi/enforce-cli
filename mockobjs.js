cbe9e375a3ab78fe1f16c6a539cecbe9e83c0371
n4y0n <e.montada0@gmail.com>
2018-06-22 18:45:52 +0100

    Edit camera mock

--- mockobjs.js
+++ mockobjs.js
@@ -1,28 +1,26 @@
 module.exports = {
-    camera: {
-        kill() {},
-        start() {},
-        send(c) {},
-        stop() {}
-    },
-    pilot: {
-        disableAutopilot() {},
-        enableAutopilot() {},
-        status() {}
-    },
-    motors: {
-        setRight(motorState) {},
-        setLeft(motorState) {},
-        getStatus() {}
-    },
-    sensors: {
-        gpsOff() {},
-        gpsOn() {},
-        thpOff() {},
-        thpOn() {},
-        imuOff() {},
-        imuOn() {},
-        status() {}
-    },
-    targeter: {}
+	camera: {
+		start() {},
+		stop() {}
+	},
+	pilot: {
+		disableAutopilot() {},
+		enableAutopilot() {},
+		status() {}
+	},
+	motors: {
+		setRight(motorState) {},
+		setLeft(motorState) {},
+		getStatus() {}
+	},
+	sensors: {
+		gpsOff() {},
+		gpsOn() {},
+		thpOff() {},
+		thpOn() {},
+		imuOff() {},
+		imuOn() {},
+		status() {}
+	},
+	targeter: {}
 }
